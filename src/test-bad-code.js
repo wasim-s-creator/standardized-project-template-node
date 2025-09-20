4718db7f9fcdecbc1da8ec8feeae722f535b3fdf
wasim-s-creator <sayyadwasim99@gmail.com>
2025-09-20 16:59:14 +0530

    added some code

---
+++ src/test-bad-code.js
@@ -0,0 +1,21 @@
+﻿function badFunction() {
+    let unusedVar = 42
+    console.log("Hello world")
+}
+
+var unusedVariable = "This variable is never used"
+
+function spacingIssues ( )  {
+   console.log ("Spacing is bad" );
+}
+
+const anotherBadFunction = () => {
+        var x=1+2+3+4+5
+        console.log(x )
+}
+
+if(true)console.log("No braces");
+
+badFunction();
+anotherBadFunction();
+spacingIssues();
