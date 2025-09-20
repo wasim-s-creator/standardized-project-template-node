a0201ab77595f976bb28fb6ab794f7f1cc20359c
wasim-s-creator <sayyadwasim99@gmail.com>
2025-09-20 18:32:47 +0530

    chore: normalize all line endings to lf as per .gitattributes

---
+++ src/bad.js
@@ -0,0 +1,8 @@
+// function testFunc() {
+//   var unused = 5;
+//   console.log('Hello world'); // no-console is off, will not warn
+//   if (true) {
+//     return 'Missing semicolons and inconsistent quotes';
+//   }
+// }
+// testFunc();
