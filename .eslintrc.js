851b85cd65018b8fffc71d2693d5dbcaa347a7e2
wasim-s-creator <75060824+wasim-s-creator@users.noreply.github.com>
2025-09-20 16:11:32 +0530

    add ESLint configuration for code quality checks

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  env: {
+    browser: false,
+    node: true,
+    es2021: true,
+    mocha: true
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'commonjs'
+  },
+  rules: {
+    // Error prevention
+    'no-undef': 'error',
+    'no-unused-vars': ['warn', { 
+      'argsIgnorePattern': '^_',
+      'varsIgnorePattern': '^_' 
+    }],
+    'no-console': 'warn',
+    
+    // Code style
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    
+    // Best practices
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'no-trailing-spaces': 'error',
+    'eol-last': 'error',
+    
+    // Node.js specific
+    'no-process-exit': 'warn',
+    'no-sync': 'warn'
+  },
+  globals: {
+    'process': 'readonly',
+    'Buffer': 'readonly',
+    '__dirname': 'readonly',
+    '__filename': 'readonly',
+    'module': 'readonly',
+    'require': 'readonly',
+    'exports': 'readonly',
+    'global': 'readonly'
+  }
+};
